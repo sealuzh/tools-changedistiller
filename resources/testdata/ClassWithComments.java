44e27312e6b97676a570d3835aebd096657fa0ee
Beat Fluri <fluri@ifi.uzh.ch>
2011-02-17 16:29:22 +0100

    RESOLVED - task 6: Extract comments from ecj parser http://titan.ifi.uzh.ch/issues/show/6

---
+++ resources/testdata/ClassWithComments.java
@@ -0,0 +1,14 @@
+/**
+ * A class with comments.
+ *
+ * @author Beat Fluri
+ */
+public class ClassWithComments {
+
+    public ClassWithComments() {
+        // a simple method invocation
+        toString();
+    }
+    
+    /* no more methods */
+}
