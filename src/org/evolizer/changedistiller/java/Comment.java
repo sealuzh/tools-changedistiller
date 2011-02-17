00ac7f78e94d463363845ee53db4ed73fa47ff2f
Beat Fluri <fluri@ifi.uzh.ch>
2011-02-17 16:22:58 +0100

    RESOLVED - task 5: Create AST nodes from comments http://titan.ifi.uzh.ch/issues/show/5

---
+++ src/org/evolizer/changedistiller/java/Comment.java
@@ -0,0 +1,60 @@
+package org.evolizer.changedistiller.java;
+
+import org.eclipse.jdt.internal.compiler.ast.ASTNode;
+
+/**
+ * AST node representing any kind of comment.
+ * 
+ * @author Beat Fluri
+ */
+public class Comment extends ASTNode {
+
+    private String fComment;
+    private CommentType fType;
+
+    /**
+     * Creates a new comment.
+     * 
+     * @param type
+     *            of the comment
+     * @param sourceStart
+     *            in the source file
+     * @param sourceEnd
+     *            in the source file
+     * @param comment
+     *            as text
+     */
+    public Comment(CommentType type, int sourceStart, int sourceEnd, String comment) {
+        fType = type;
+        this.sourceStart = sourceStart;
+        this.sourceEnd = sourceEnd;
+        fComment = comment;
+    }
+
+    @Override
+    public StringBuffer print(int indent, StringBuffer output) {
+        printIndent(indent, output);
+        output.append(fComment);
+        return output;
+    }
+
+    public CommentType getType() {
+        return fType;
+    }
+
+    public String getComment() {
+        return fComment;
+    }
+
+    /**
+     * Type of comments that java provides.
+     * 
+     * @author Beat Fluri
+     */
+    public enum CommentType {
+        LINE_COMMENT,
+        BLOCK_COMMENT,
+        JAVA_DOC
+    }
+
+}
