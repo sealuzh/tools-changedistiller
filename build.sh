27fa9da8376553744fd7f03a186a94bd7b30417c
Michael Wuersch <wuersch@ifi.uzh.ch>
2013-03-11 17:12:25 +0100

    small improvement to the build.sh file: './build.sh -i' will now install to the local mvn repo, whereas no params will install to /tmp

--- build.sh
+++ build.sh
@@ -1,1 +1,17 @@
-mvn clean install -Dmaven.repo.local=/tmp/hwk.localrepo
+#!/bin/sh
+
+P1="-i"
+LOCALREPO=/tmp/seal.localrepo
+
+if [ $# -ne 0 ] ; then
+	if [ $1 = $P1 ] ; then
+		echo "Installing to ~/.m2"
+		mvn clean install
+	else
+		echo "unrecognized parameter: $1"
+	fi
+else
+	echo "Installing to $LOCALREPO"
+	
+	mvn clean install -Dmaven.repo.local=$LOCALREPO
+fi
